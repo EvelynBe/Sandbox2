9c9b402d04e9b5db658658dc042ce215ec10f828
Evelyn Benitez <evelyn.benitez.ponce@gmail.com>
2023-01-23 10:58:22 -0600

    first commit

---
+++ practica04.php
@@ -0,0 +1,10 @@
+<?php
+#Esto es un comentario
+#Como declarar una varable numerica
+$ValorNum=5;
+echo"El valor de la variable numerca es:$ValorNum";
+
+#Variable tipo texto o cadena
+$texto="Jose Alberto Contreras";
+echo"<br>El contenido de la cadena es mi nombre:$texto";
+?>
